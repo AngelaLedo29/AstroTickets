5ffab03e4f7733e81650f53e53f736a3b997d491
AngelaLedo29 <angelaledo29@gmail.com>
2025-12-15 16:33:37 +0100

    Modificación de la página de los events

--- src/components/EventsTickets.jsx
+++ src/components/EventsTickets.jsx
@@ -20,29 +20,32 @@ export default function EventsTickets() {
     };
 
     return (
-        <main className="flex-1 container mx-auto px-8 py-12 font-magic max-w-6xl">
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 px-6 mb-2">
+        <main className="flex-1 container mx-auto px-8 py-12 max-w-6xl">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 px-6 mb-2">
                 {tickets.map(ticket => (
-                    <div key={ticket.id} className="bg-purple-400 p-6 rounded-xl shadow-lg border border-yellow-400 flex flex-col justify-between">
-                        <h5 class="mb-3 text-2xl font-bold text-yellow-400 leading-8">{ticket.name}</h5>
-                        <p class="text-yellow-200 mb-2">Precio: {ticket.price}€</p>
-                        <p className="text-yellow-300 mt-2 mb-2"><em>Stock: {ticket.stock}</em></p>
+                    <div key={ticket.id} className="p-6 rounded-xl shadow-lg text-center bg-white
+                            transform transition duration-300
+                            hover:scale-[1.03] hover:shadow-xl
+                            border border-gray-200">
+                        <h5 className="mb-3 text-2xl font-bold text-yellow-600 leading-8">{ticket.name}</h5>
+                        <p className="text-yellow-600 mb-2">Precio: {ticket.price}€</p>
+                        <p className="text-yellow-500 mt-2 mb-2"><em>Stock: {ticket.stock}</em></p>
 
-                        <div className="flex items-center justify-between">
+                        <div className="flex items-center justify-center">
                             <button
                             onClick={() => handleBuy(ticket.id)}
                             disabled={ticket.stock === 0}
-                            className={`px-4 py-2 rounded-md font-bold text-sm focus:outline-none transition-colors
+                            className={`w-full px-4 py-2 rounded-md font-bold text-sm focus:outline-none transition-colors
                                 ${ticket.stock === 0 
                                     ? "bg-gray-400 text-gray-700 cursor-not-allowed opacity-60" 
-                                    : "bg-yellow-400 text-purple-900 hover:bg-yellow-300"
+                                    : "bg-yellow-500 text-gray-900 hover:bg-yellow-400"
                                 }
                             `}
                             >
-                            {ticket.stock > 0 ? "Comprar" : "Agotado"}
+                                {ticket.stock > 0 ? "Comprar" : "Agotado"}
                             </button>
 
-                            <span className="text-yellow-200 font-semibold">
+                            <span className="text-yellow-600 font-semibold">
                                 {ticket.stock === 0 ? "Sin stock" : ""}
                             </span>
                         </div>
